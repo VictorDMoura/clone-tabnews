51928b8bcebd10634df32f2def834770827d7999
Victor de Moura <76460793+VictorDMoura@users.noreply.github.com>
2024-01-21 20:47:47 +0000

    adiciona scripts `lint:check` `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 function Home() {
-    return (
-        <div>
-            <h1>Olá bonita 😍</h1>
-            <p>Passando aqui para falar o qt vc é especial</p>
-            <p>Mas chateado pq vc vai me fazer ir no casamento rum 😢</p>
-            <p>Amo vc demais ❤️</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Olá bonita 😍</h1>
+      <p>Passando aqui para falar o qt vc é especial</p>
+      <p>Mas chateado pq vc vai me fazer ir no casamento rum 😢</p>
+      <p>Amo vc demais ❤️</p>
+    </div>
+  );
 }
 
 export default Home;
