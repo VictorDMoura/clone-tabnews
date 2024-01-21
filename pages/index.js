105b432c840dc4a06b70cf550510a74afde08bb8
Victor de Moura <76460793+VictorDMoura@users.noreply.github.com>
2024-01-21 13:19:32 +0000

    Nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Olá, bonita 😜</h1>
+    return (
+        <div>
+            <h1>Olá bonita 😍</h1>
+            <p>Passando aqui para falar o qt vc é especial</p>
+            <p>Mas chateado pq vc vai me fazer ir no casamento rum 😢</p>
+            <p>Amo vc demais ❤️</p>
+        </div>
+    )
 }
 
 export default Home;
